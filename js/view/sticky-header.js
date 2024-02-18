7ff0c071ce4bfb1e0e84646509bb678de7e635a9
Maria Lisitsa <sokalslash@mail.ru>
2024-02-18 19:35:46 +0300

    feat: change logo

--- js/view/sticky-header.js
+++ js/view/sticky-header.js
@@ -1,7 +1,10 @@
 const header = document.querySelector('.header');
+const logoButton = document.querySelector('.logo-button__img');
 
 const findCurrentScroll = () => window.pageYOffset;
 const findHideClass = () => header.classList.contains('hide');
+
+logoButton.addEventListener('click', () => header.classList.remove('hide'));
 
 let lastScroll = 0;
 
