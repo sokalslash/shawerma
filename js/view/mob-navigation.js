3840dbad1905526bbc09946e05df6cf827ac98d4
Lisica-M <sokalslash@mail.ru>
2024-02-27 09:50:53 +0300

    fix: fix the height of the header in the mobile version

--- js/view/mob-navigation.js
+++ js/view/mob-navigation.js
@@ -1,12 +1,10 @@
 const navMenu = document.querySelector('.header__nav');
 const burgerBtn = document.querySelector('.nav-toggle');
-const burgerBasketIcons = document.querySelector('.basket-icons');
 const body = document.querySelector('.body');
 
 burgerBtn.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   burgerBtn.classList.toggle('active');
-  burgerBasketIcons.classList.toggle('hide');
   body.classList.toggle('nav-menu-open');
 })
 
