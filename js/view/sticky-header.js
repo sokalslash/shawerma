3367644e85586dd2255f22c4df52a4878793211b
Lisica-M <sokalslash@mail.ru>
2024-02-29 16:32:27 +0300

    feat: implement stiky-header through BEM

--- js/view/sticky-header.js
+++ js/view/sticky-header.js
@@ -2,17 +2,17 @@ const header = document.querySelector('.header');
 const logoButton = document.querySelector('.logo-button__img');
 
 const findCurrentScroll = () => window.pageYOffset;
-const findHideClass = () => header.classList.contains('hide');
+const findHideClass = () => header.classList.contains('header--hidden');
 
-logoButton.addEventListener('click', () => header.classList.remove('hide'));
+logoButton.addEventListener('click', () => header.classList.remove('header--hidden'));
 
 let lastScroll = 0;
 
 function addStickyHeader() {
   if (findCurrentScroll() > lastScroll && !findHideClass()) {
-    header.classList.add('hide');
+    header.classList.add('header--hidden');
   } else if (findCurrentScroll() < lastScroll && findHideClass()){
-    header.classList.remove('hide');
+    header.classList.remove('header--hidden');
   }
   lastScroll = findCurrentScroll();
 }
