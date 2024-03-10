7494b5efd8c4a3e5650665d88cd7aca4312f594d
Maria Lisitsa <sokalslash@mail.ru>
2024-03-10 22:40:17 +0300

    feat-animation: split animation into media queries

--- js/view/animation.js
+++ js/view/animation.js
@@ -12,18 +12,7 @@ function animation() {
         toggleActions: 'play none none reverse'
       }
     })
-  
-    gsap.from('.shawarma__item', {
-      opacity: 0,
-      yPercent: 100,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: '.shawarma__list',
-        start: 'top bottom',
-        toggleActions: 'play none none reverse'
-      }
-    })
-  
+
     gsap.from('.burgers__inner', {
       scale: 0,
       duration: 0.8,
@@ -33,26 +22,82 @@ function animation() {
         toggleActions: 'play none none reverse'
       }
     })
-    
-    gsap.from('.snacks__item', {
-      opacity: 0,
-      yPercent: 50,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: '.snacks__list',
-        start: 'top bottom',
-        toggleActions: 'play none none reverse'
-      }
-    })
+
+    let mobileScreen = gsap.matchMedia();
+    mobileScreen.add('(min-width: 320px)', () => {
+
+      gsap.from('.shawarma__item', {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.8,
+        scrollTrigger: {
+          trigger: '.shawarma__list',
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+          markers: true
+        }
+      })
   
-    gsap.from('.drinks__item', {
-      opacity: 0,
-      stagger: 0.2,
-      scrollTrigger: {
-        trigger: '.drinks__list',
-        start: 'top bottom',
-        toggleActions: 'play none none reverse'
-      }
+      gsap.from('.snacks__item', {
+        opacity: 0,
+        yPercent: 50,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: '.snacks__list',
+          start: 'top 80%',
+          toggleActions: 'play none none reverse'
+        }
+      })
+  
+      gsap.from('.drinks__item', {
+        opacity: 0,
+        yPercent: 50,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: '.drinks__list',
+          start: 'top 80%',
+          toggleActions: 'play none none reverse'
+        }
+      })
+    })
+
+
+    let desktoptopScreen = gsap.matchMedia();
+    desktoptopScreen.add('(min-width: 591px)', () => {
+
+      gsap.from('.shawarma__item', {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: '.shawarma__list',
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+          markers: true
+        }
+      })
+  
+      gsap.from('.snacks__item', {
+        opacity: 0,
+        yPercent: 50,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: '.snacks__list',
+          start: 'top 80%',
+          toggleActions: 'play none none reverse'
+        }
+      })
+  
+      gsap.from('.drinks__item', {
+        opacity: 0,
+        yPercent: 50,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: '.drinks__list',
+          start: 'top 80%',
+          toggleActions: 'play none none reverse'
+        }
+      })
     })
   }
 
