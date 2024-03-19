3a9d49c9b8700e9465038ef4969980364750fb69
Maria Lisitsa <sokalslash@mail.ru>
2024-03-19 23:20:02 +0300

    feat: add the adaptation of the basket- block to the low screen size

--- js/view/adapt-screen.js
+++ js/view/adapt-screen.js
@@ -1,5 +1,6 @@
 const navList = document.querySelector('.nav__list');
 const arrowUp = document.querySelector('.up-arrow');
+const basket = document.querySelector('.basket-content');
 
 function fix100vh() {
     let vh = document.documentElement.clientHeight * 0.01;
@@ -11,11 +12,13 @@ function fix100vh() {
   
     if (fullHeight < 320) {
       navList.classList.add('nav__list--low-screen');
-      arrowUp.classList.add('no-full')
+      arrowUp.classList.add('no-full');
+      basket.classList.add('header__basket-content--low-screen');
     } else {
       if (navList.classList.contains('nav__list--low-screen')) {
         navList.classList.remove('nav__list--low-screen');
         arrowUp.classList.remove('no-full');
+        basket.classList.remove('header__basket-content--low-screen');
       }
     }
   }
